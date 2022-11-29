0c61b209e2f846b6221dcf6d007150d85c0ab941
Norbert Gila <gila.nrbrt@gmail.com>
2022-11-29 23:33:52 +0100

    implemented ImageService

--- src/main/java/com/codecool/fileshare/service/ImageService.java
+++ src/main/java/com/codecool/fileshare/service/ImageService.java
@@ -2,13 +2,16 @@ package com.codecool.fileshare.service;
 
 import com.codecool.fileshare.dto.ImageDataDTO;
 import com.codecool.fileshare.dto.ImageUpdateDTO;
+import com.codecool.fileshare.model.Image;
 import com.codecool.fileshare.repository.ImageRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
+import java.io.IOException;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 public class ImageService {
@@ -18,37 +21,40 @@ public class ImageService {
     private ImageRepository imageRepository;
 
     public List<ImageDataDTO> getAll(String user) {
-        //get owned by user.
-        //help: imgurl = System.getenv("url") + "/api/artwork/" + id + "." +image.getExtension();
-        //TODO
-        throw new RuntimeException("Not yet implemented!");
+        return imageRepository.getAll(user).stream()
+                .map(i -> new ImageDataDTO(i.getId(), i.getTitle(), i.getDescription(),
+                        System.getenv("url") + "/api/artwork/" + i.getId() + "." + i.getExtension()))
+                .collect(Collectors.toList());
     }
 
     public boolean checkOwner(String owner, String id) {
-        //TODO
-        throw new RuntimeException("Not yet implemented!");
+        return imageRepository.checkOwner(owner, id);
     }
 
     public void delete(String id, String owner) {
-        //TODO
-        throw new RuntimeException("Not yet implemented!");
-
+        imageRepository.delete(id, owner);
     }
 
     public void updateCategory(String id, ImageUpdateDTO imageUpdateDTO, String owner) {
-        //TODO
-        throw new RuntimeException("Not yet implemented!");
+        imageRepository.updateImage(id, imageUpdateDTO.getTitle(), imageUpdateDTO.getDescription(), owner);
     }
 
     public byte[] getImageFile(String filename) {
         //help: filename is for example 41d6608d-0803-4239-9235-09f902fbf705.jpg
-        //TODO
-        throw new RuntimeException("Not yet implemented!");
+        String id = filename.split(".")[0];
+        return imageRepository.getImageFile(id);
     }
 
     public String storeFile(MultipartFile file, String title, String description, String owner) {
         //help: filename is for example 41d6608d-0803-4239-9235-09f902fbf705.jpg
-        //TODO
-        throw new RuntimeException("Not yet implemented!");
+        try {
+            String extension = file.getOriginalFilename().split(".")[1];
+            String imageId = imageRepository.storeImageFile(title, description, owner, file.getBytes(), extension);
+            return imageId + "." + extension;
+        } catch (IOException|NullPointerException e) {
+            e.printStackTrace();
+        }
+
+        return null; //TODO what if its null?
     }
 }
