b80c9eb8ae81b921128c706c70fefd197625e014
T.Sanyi <s.truczkai@gmail.com>
2022-11-29 18:06:01 +0100

    implement save()

--- src/main/java/com/codecool/fileshare/repository/UserJdbcRepository.java
+++ src/main/java/com/codecool/fileshare/repository/UserJdbcRepository.java
@@ -23,7 +23,7 @@ public class UserJdbcRepository implements UserRepository{
             st.setString(1, email);
             ResultSet rs = st.executeQuery();
             if (rs.next()){
-                //TODO is the password encrypted here or not, how do we need it?
+                //ATTENTION password is encrypted here, how do we need it?
                 appUser = new AppUser(email, rs.getString(2));
             }
         } catch (SQLException e) {
@@ -34,8 +34,15 @@ public class UserJdbcRepository implements UserRepository{
 
     @Override
     public void save(AppUser appUser) throws UserAlreadyExistsException {
-        //TODO
-        throw new RuntimeException("Not yet implemented!");
+        final String SQL = "INSERT INTO app_user(email, password) VALUES(?, ?);";
+        try (Connection con = DriverManager.getConnection(DB_URL, DB_USERNAME, DB_PASSWORD)) {
+            PreparedStatement st = con.prepareStatement(SQL);
+            st.setString(1, appUser.getEmail());
+            st.setString(2, appUser.getPassword());
+            ResultSet rs = st.executeQuery();
+        } catch (SQLException e) {
+            throw new UserAlreadyExistsException();
+        }
     }
 
     @Override
