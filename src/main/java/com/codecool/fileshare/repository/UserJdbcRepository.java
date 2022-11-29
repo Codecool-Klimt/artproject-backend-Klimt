6e2777d3d7b1c4f07d669a1cb589bfe83ee58dd7
T.Sanyi <s.truczkai@gmail.com>
2022-11-29 17:52:29 +0100

    implement findByUsername()

--- src/main/java/com/codecool/fileshare/repository/UserJdbcRepository.java
+++ src/main/java/com/codecool/fileshare/repository/UserJdbcRepository.java
@@ -8,14 +8,28 @@ import java.sql.*;
 import java.util.ArrayList;
 import java.util.List;
 
+import static com.codecool.fileshare.repository.DbConstants.*;
+
 @Repository
 public class UserJdbcRepository implements UserRepository{
 
 
     @Override
-    public AppUser findByUsername(String username) {
-        //TODO
-        throw new RuntimeException("Not yet implemented!");
+    public AppUser findByUsername(String email) {
+        final String SQL = "SELECT email, password FROM app_user WHERE email = ?;";
+        AppUser appUser = null;
+        try (Connection con = DriverManager.getConnection(DB_URL, DB_USERNAME, DB_PASSWORD)) {
+            PreparedStatement st = con.prepareStatement(SQL);
+            st.setString(1, email);
+            ResultSet rs = st.executeQuery();
+            if (rs.next()){
+                //TODO is the password encrypted here or not, how do we need it?
+                appUser = new AppUser(email, rs.getString(2));
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return appUser;
     }
 
     @Override
