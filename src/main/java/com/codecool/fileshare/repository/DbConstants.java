79a1e47b4c03968ce39c02b7ff5282978f527721
T.Sanyi <s.truczkai@gmail.com>
2022-11-29 17:25:57 +0100

    add DB_URL constant to DbConstants

--- src/main/java/com/codecool/fileshare/repository/DbConstants.java
+++ src/main/java/com/codecool/fileshare/repository/DbConstants.java
@@ -5,6 +5,7 @@ public class DbConstants {
     final static String PORT = System.getenv("port");
     final static String DB_NAME = System.getenv("dbname");
     final static String DB_USERNAME = System.getenv("dbusername");
-    final static String DB_PASSWORD =System.getenv("dbpassword");
+    final static String DB_PASSWORD = System.getenv("dbpassword");
     final static String TOKEN_KEY = System.getenv("tokenkey");
+    final static String DB_URL = "jdbc:postgresql://" + HOST + ":" + PORT + "/" + DB_NAME;
 }
